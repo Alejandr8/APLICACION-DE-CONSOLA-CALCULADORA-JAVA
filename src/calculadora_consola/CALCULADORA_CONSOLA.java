c31890f395f86444565188769be4a3cde4303646
Alejandro Garcia <46765816+Alejandr8@users.noreply.github.com>
2021-01-30 16:52:48 -0500

    Update CALCULADORA_CONSOLA.java

--- src/calculadora_consola/CALCULADORA_CONSOLA.java
+++ src/calculadora_consola/CALCULADORA_CONSOLA.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//APLICACION DE CONSOLA: CALCULADORA
+
 package calculadora_consola;
 import java.util.Scanner;
 
